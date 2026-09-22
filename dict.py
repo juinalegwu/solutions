13d7933a02c30108207e0fc73308c241de97b70a
juinalegwu <inalegwujuliet2@gmail.com>
2026-09-22 16:44:45 +0100

    dict.py

---
+++ dict.py
@@ -0,0 +1,21 @@
+student = {
+    "name": "Alice",
+    "age": 20,
+    "courses": ["math", "Physics"]
+}
+print(student["name"])
+print(student["age"])
+
+
+student = {
+    "name": "Alice",
+    "age": 20,
+    "courses": ["Math", "Physics"]
+}
+
+
+student["gpa"] = 3.9
+
+student["age"] = 21
+
+print(student)
